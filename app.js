fe9c754d9edee6cc3bdb49a1286861fb26186187
Kenny T <ktrionfo@integrat[C[core.com>
2015-10-30 21:38:27 +0000

    test form

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
-}]);
+angularApp.controller('mainController', ['$scope', '$log', '$resource', function ($scope, $log, $resource) {
+	console.log($resource );
+	var things = [1,2,3];
+	console.log(things );
 
+}]);
 
 
