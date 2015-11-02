324691cc20cef7540eb01c2ea65fb15e076a2407
Kenny T <ktrionfo@integracore.com>
2015-11-02 23:28:24 +0000

    playing with services

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 var angularApp = angular.module('angularApp', ['ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$log', '$resource', function ($scope, $log, $resource) {
+angularApp.controller('mainController', ['$scope', '$log', '$resource', '$timeout', function ($scope, $log, $resource, $timeout) {
 	console.log($resource );
 	var things = [1,2,3];
 	console.log(things );
+
+	$scope.name = 'Kenny';
+
+	$timeout(function() {
+		$scope.name = 'Everybody';
+	}, 3000);
+
 
 }]);
 
