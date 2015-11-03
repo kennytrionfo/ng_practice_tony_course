32915220e9262d9eb10bf8bb32bbc04d4e435921
Kenny T <ktrionfo@integracore.com>
2015-11-03 00:22:33 +0000

    more playing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var angularApp = angular.module('angularApp', ['ngResource']);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$log', '$resource', '$timeout', function ($scope, $log, $resource, $timeout) {
+angularApp.controller('mainController', ['$scope', '$log', '$resource', '$timeout', '$filter',  function ($scope, $log, $resource, $timeout, $filter) {
 	console.log($resource );
 	var things = [1,2,3];
 	console.log(things );
@@ -13,6 +13,10 @@ angularApp.controller('mainController', ['$scope', '$log', '$resource', '$timeou
 		$scope.name = 'Everybody';
 	}, 3000);
 
+	$scope.handle = '';
+	$scope.lowercasehandle = function() {
+		return $filter('lowercase')($scope.handle);
+	};
 
 }]);
 
