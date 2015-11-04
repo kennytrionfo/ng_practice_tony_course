c149064550e1302f9fea9d6017f890f130f8fd85
Kenny T <ktrionfo@integracore.com>
2015-11-04 18:29:07 +0000

    updated steps for twittister app

--- twitter.js
+++ twitter.js
@@ -3,7 +3,6 @@ var angularApp = angular.module('angularApp', []);
  
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', function ($scope ) {
-console.log("Don't be a cottenheaded ninimugin " );
 	$scope.handle = '';
 	// $scope.lowercasehandle = function() {
 	// 	return $filter('lowercase')($scope.handle);
