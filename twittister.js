f1f09ac47fd6cf4f318e6292d1effdfc786faf14
Kenny T <ktrionfo@integracore.com>
2015-11-04 23:57:48 +0000

    added finals steps for adding service

--- twittister.js
+++ twittister.js
@@ -1,5 +1,12 @@
 var twittisterApp = angular.module('twittisterApp', []);
 
-twittisterApp.controller('stringController', ['$scope', function ($scope) {
+twittisterApp.controller('stringController', ['$scope', '$timeout', function ($scope, $timeout) {
 	$scope.name = 'Kenny Ken ken';
+	$timeout(function() {
+		$scope.name = "Willy Wonka"
+	}, 2000);
+}]);
+
+twittisterApp.controller('handleController', ['$scope', function ($scope) {
+	$scope.handle = '';
 }]);
