906a563d5ff955f0942e84779e6b841f6bd6fe18
Kenny T <ktrionfo@integracore.com>
2015-11-04 23:35:02 +0000

    changing up controller

--- twitter.js
+++ twitter.js
@@ -1,12 +1,11 @@
-// MODULE
 var angularApp = angular.module('angularApp', []);
  
-// CONTROLLERS
 angularApp.controller('mainController', ['$scope', function ($scope ) {
+	console.log("the seal goes owe owe owe " );
 	$scope.handle = '';
+	$scope.name = "MIBOSO "
 	// $scope.lowercasehandle = function() {
 	// 	return $filter('lowercase')($scope.handle);
 	// };
-	
 
 }]);
--- twittister.js
+++ twittister.js
@@ -1,3 +1,5 @@
-var twittesterApp = angular.module('twittesterApp', []);
+var twittisterApp = angular.module('twittisterApp', []);
 
-twittesterApp.controller = 
+twittisterApp.controller('stringController', ['$scope', function ($scope) {
+	$scope.name = 'Kenny Ken ken';
+}]);
