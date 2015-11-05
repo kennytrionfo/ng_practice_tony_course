4f941c5bda81269022f4e41846f9a80607eabb2c
Kenny T <ktrionfo@integracore.com>
2015-11-05 18:48:07 +0000

    steps updated

--- twiffer.js
+++ twiffer.js
@@ -2,5 +2,8 @@ var angularApp = angular.module('twifferApp', []);
 
 angularApp.controller('stringController', ['$scope',  function ($scope) {
 	$scope.randomPhrase = 'If the elevator tries to break you down, go crazy! ';
+}]);
 
+angularApp.controller('handleController', ['$scope',  function ($scope) {
+	$scope.handle = ' ';
 }]);
