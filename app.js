a60c80e9bbf9444622dbad22446137ee9047e958
Kenny T <ktrionfo@integracore.com>
2015-11-05 23:39:30 +0000

    last few notes

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var angularApp = angular.module('angularApp', ['ngResource']);
  
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', '$log', '$resource', '$timeout', '$filter',  function ($scope, $log, $resource, $timeout, $filter) {
-	console.log($resource );
-	var things = [1,2,3];
-	console.log(things );
-
 	$scope.name = 'Kenny';
 
 	$timeout(function() {
