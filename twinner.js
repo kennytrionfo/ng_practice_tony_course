9258a1c4186371ac290ce71c75459bb692d766a7
Kenny T <ktrionfo@integracore.com>
2015-11-09 20:01:43 +0000

    finnished twinner app

--- twinner.js
+++ twinner.js
@@ -1,1 +1,9 @@
-	 
+var angularApp = angular.module('twinnerApp', []);
+
+angularApp.controller('stringController', ['$scope', '$timeout', function ($scope, $timeout ) {
+	$scope.phrase = "Don't be a cottenheaded ninimugin ";
+	$scope.handle = ''; 
+	$timeout(function() {
+		$scope.phrase = "What would I do without your sweet mouth ";
+	}, 2000);
+}]);
