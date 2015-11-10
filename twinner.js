3b2e94a1534756fa2c60139dfc660261d3ed3355
Kenny T <ktrionfo@integracore.com>
2015-11-10 18:58:05 +0000

    setting up ng route module

--- twinner.js
+++ twinner.js
@@ -2,8 +2,12 @@ var angularApp = angular.module('twinnerApp', []);
 
 angularApp.controller('stringController', ['$scope', '$timeout', function ($scope, $timeout ) {
 	$scope.phrase = "Don't be a cottenheaded ninimugin ";
-	$scope.handle = ''; 
 	$timeout(function() {
 		$scope.phrase = "What would I do without your sweet mouth ";
 	}, 2000);
+
+angularApp.controller('handleController', ['$scope', function ($scope) {
+	$scope.handle = ''; 
+	}]);
+
 }]);
