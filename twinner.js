e442d554dad7bb8593e447585c2912e20b857b09
Kenny T <ktrionfo@integracore.com>
2015-11-10 20:49:23 +0000

    added pages

--- twinner.js
+++ twinner.js
@@ -1,13 +1,27 @@
-var angularApp = angular.module('twinnerApp', []);
+var angularApp = angular.module('twinnerApp', ['ngRoute']);
+
+angularApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/main.html', 
+			controller: 'handleController'
+		})
+		.when('/second', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+});
 
 angularApp.controller('stringController', ['$scope', '$timeout', function ($scope, $timeout ) {
 	$scope.phrase = "Don't be a cottenheaded ninimugin ";
 	$timeout(function() {
 		$scope.phrase = "What would I do without your sweet mouth ";
 	}, 2000);
+}]);
 
 angularApp.controller('handleController', ['$scope', function ($scope) {
 	$scope.handle = ''; 
-	}]);
+}]);
 
+angularApp.controller('secondController',  ['$scope', function ($scope) {
 }]);
