e2a9ff0f6fb3847b945cf2e76f741664de193f77
Kenny T <ktrionfo@integracore.com>
2016-01-28 23:00:53 +0000

    added controlller to test ang

--- tipper_app/tipper_app.js
+++ tipper_app/tipper_app.js
@@ -1,6 +1,12 @@
 (function(){
 
-var app = angular.module('tipperApp', []);
+	var app = angular.module('tipperApp', []);
+
+/////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	app.controller('PhraseCtrl', function($scope){
+		$scope.phrase = "I'm a random phrase";
+		$scope.handle = ""
+	});
 
 
 })();
