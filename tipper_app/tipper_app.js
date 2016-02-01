6194adb69d356939ad3d41cd05a3345459482677
Kenny T <ktrionfo@integracore.com>
2016-02-01 18:40:04 +0000

    adding a data file to use http

--- tipper_app/tipper_app.js
+++ tipper_app/tipper_app.js
@@ -1,6 +1,6 @@
 (function(){
 
-	var app = angular.module('tipperApp', []);
+	var app = angular.module('tipperApp', ['ngResource']);
 
 /////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	app.controller('PhraseCtrl', function($scope, $timeout ){
@@ -10,7 +10,7 @@
 		}, 2000);
 	});
 
-	function HandleCtrl($scope, $filter){
+	function HandleCtrl($scope, $filter, $http, $resource){
 		$scope.handle = "";
 		$scope.lowercasehandle = function(){
 			return $filter('uppercase')($scope.handle);
@@ -19,6 +19,22 @@
 		$scope.alertClick = function(){
 			alert("Hey there!");
 		};
+
+///////////////========This was trying to use $http Service. Couldn't get to work=======\\\\\\\\\\\\\\\\\
+		$scope.data = $http.get('data.json')
+			.success(function(result){
+				return result;
+			})
+			.error(function(data, error){
+				console.log(data );
+			})
+		console.log($scope.data );
+/////////////////////////========End of $http Service stuff=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+		$scope.fakeData = $resource('data.json').get().promise.then(function(data){
+			$scope.names = data;
+		})
+		console.log($scope.fakeData );
+
 	}
 	app.controller('HandleCtrl', HandleCtrl);
 
