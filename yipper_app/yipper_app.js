d96b6d178ed7e7cfb99fe9999541e97dc3f515a7
k[C[C[C[C[C[C[C[C ki[C[C[C[C[C[C[C[C[C[C[C[Dionfo <ktrionfo@integracore.com>
2016-05-03 20:12:02 +0000

    added handle model

--- yipper_app/yipper_app.js
+++ yipper_app/yipper_app.js
@@ -4,8 +4,14 @@
 	var app = angular.module('yipperApp', []);
 
 /////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
-	app.controller('PhraseCtrl', ['$scope',  function($scope){
+	app.controller('PhraseCtrl', ['$scope', '$timeout',  function($scope, $timeout){
 		$scope.randomPhrase = "This life is more than just a read-through. ";
+		$scope.handle = ' ';
+		$timeout(function(){
+			$scope.randomPhrase = "I should show up after 2 seconds!";
+		}, 2000);
+
+
 	}]);
 
 
